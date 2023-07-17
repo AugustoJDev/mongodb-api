cc0293ad8830e12dcff3f0315b6f95986e08aa58
WinchesterDev <22821726-WinchesterDev@users.noreply.replit.com>
2023-07-17 04:48:57 +0000

    Initial commit

---
+++ database/connect.js
@@ -0,0 +1,174 @@
+const {
+    MongoClient
+} = require('mongodb');
+
+class DatabaseConnection {
+    constructor(uri) {
+        this.uri = uri;
+        this.client = null;
+        this.database = null;
+    }
+
+    async open() {
+        try {
+            this.client = new MongoClient(this.uri);
+            await this.client.connect();
+            this.database = this.client.db(process.env.DBNAME);
+        } catch (error) {
+            console.error('Error connecting to database:', error);
+        }
+    }
+
+    async close() {
+        try {
+            await this.client.close();
+        } catch (error) {
+            console.error('Error closing database connection:', error);
+        }
+    }
+
+    async getDocuments(collectionName, field) {
+        try {
+            await this.open();
+
+            const collection = this.database.collection(collectionName);
+
+            let documents;
+            if (field == null) {
+                documents = await collection.find().toArray();
+            } else {
+                documents = await collection.find(field).toArray();
+            }
+
+            return documents;
+        } catch (error) {
+            console.log;
+        } finally {
+            await this.close();
+        }
+    }
+
+    async getToken(name, pass) {
+        try {
+            await this.open();
+
+            let user = await this.getDocuments("admin", {
+                login: name,
+                password: pass
+            });
+            user = user[0];
+
+            if (!user) {
+                return "Invalid User."
+            } else {
+                const token = jwt.sign({
+                    user
+                }, process.env.jwtKey);
+
+                return token;
+            }
+        } catch (error) {
+            console.log;
+        } finally {
+            await this.close();
+        }
+    };
+
+    async addValues(collectionName, data) {
+        try {
+            await this.open();
+
+            const collection = this.database.collection(collectionName);
+
+            const result = await collection.insertMany([data]);
+
+            return `Values added with success: ${result.insertedCount}`;
+        } catch (error) {
+            return `${error}`;
+        } finally {
+            await this.close();
+        }
+    };
+
+    async remValues(collectionName, data) {
+        try {
+            await this.open();
+
+            const collection = this.database.collection(collectionName);
+
+            const result = await collection.deleteOne(data);
+
+            return `${result.deletedCount} documents deleted with success`;
+        } catch (error) {
+            return `${error}`;
+        } finally {
+            await this.close();
+        }
+    };
+
+    async editValues(collectionName, filter, data) {
+        try {
+            await this.open();
+
+            const collection = this.database.collection(collectionName);
+
+            const result = await collection.updateOne(filter, data);
+
+            return `${result.modifiedCount} modified document(s) with success`;
+        } catch (error) {
+            return `${error}`;
+        } finally {
+            await this.close();
+        }
+    };
+
+    async logs(ip) {
+        try {
+            await this.open();
+
+            let requestIP = await this.getDocuments("logs", {
+                ip: ip
+            });
+
+            if (requestIP.length < 1) {
+                let data = {
+                    ip: ip,
+                    attempts: 0,
+                    dateNow: Date.now()
+                };
+
+                let result = await this.addValues("logs", data);
+
+                return console.log(`User added to login attempt count`);
+            } else {
+                let log = requestIP[0];
+
+                let blackList = await this.getDocuments("blacklist", {
+                    ip: log.ip
+                });
+
+                if (log.attempts < 3) {
+                    let result = await this.editValues("logs", {
+                        ip: log.ip
+                    }, {
+                        $set: {
+                            attempts: (log.attempts + 1)
+                        }
+                    });
+                } else {
+                    await this.addValues("blacklist", {
+                        ip: log.ip
+                    });
+                }
+            }
+        } catch (error) {
+            console.log;
+        } finally {
+            await this.close();
+        }
+    };
+};
+
+const conn = new DatabaseConnection(process.env.URL);
+
+global.conn = conn;
---
+++ server.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+require("colors");
+require("./database/connect");
+
+global.jwt = require('jsonwebtoken');
+
+app.use(express.json());
+  
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/templates/index.html"));
+});
+
+app.get("/login", async (req, res) => {
+  let token = await conn.getToken(req.query.name, req.query.pass);
+
+  if(token == "Invalid User") {
+    res.send("The login provided doesn't exist in the database.");
+  } else {
+    res.send({ token: token });
+  };
+});
+
+app.get("/getUsers", authMiddleware, async (req, res) => {
+
+  let field = req.query;
+  if(!field) field = null;
+  
+  let getUsers = await conn.getDocuments("users", field);
+  
+  res.send(getUsers)
+});
+
+app.get("/getProducts", authMiddleware, async (req, res) => {
+
+  let field = req.query;
+  if(!field) field = null;
+  
+  let getUsers = await conn.getDocuments("products", field);
+  
+  res.send(getUsers);
+});
+
+app.post("/addUser", authMiddleware, async (req, res) => {
+
+  let data = req.body;
+  
+  if(!data) {
+    res.send("Data was not entered or was sent incorrectly.");
+  } else {
+    let result = await conn.addValues("users", data);
+
+    res.send(result);
+  };
+});
+
+app.post("/remUser", authMiddleware, async (req, res) => {
+
+  let data = req.body;
+  
+  if(!data) {
+    res.send("Data was not entered or was sent incorrectly.");
+  } else {
+    let result = await conn.remValues("users", data);
+
+    res.send(result);
+  };
+});
+
+app.patch("/editUser", authMiddleware, async (req, res) => {
+
+  let data = req.body;
+  let dataToChange = {};
+
+  const filter = JSON.parse(data["query"]);
+  
+  for(keys in data) {
+    if(keys == "query") continue;
+
+    dataToChange[keys] = data[keys];
+  }
+  const update = { $set: dataToChange };
+
+  let result = await conn.editValues("users", filter, update);
+  
+  res.send(result);
+});
+
+app.post("/POST", authMiddleware, async (req, res) => {
+  res.send("Example"); 
+});
+
+app.listen(port, () => {
+  console.log("App Listening on Port: " + port)
+});
+
+async function authMiddleware(req, res, next) {
+
+  let ip = req.ip.split(":")[3];
+  
+  let bl = await conn.getDocuments("blacklist", { ip: ip });
+
+  if(bl.length > 0) return res.send("IP Blocked.");
+  
+  const token = req.headers.authorization;
+
+  req.query = req.query;
+
+  if (!token) {
+    await conn.logs(ip);
+    
+    return res.status(401).json({ mensagem: 'Token de autenticação não fornecido.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.jwtKey);
+    next();
+  } catch (error) {
+    await conn.logs(ip);
+    
+    return res.status(403).json({ mensagem: 'Token de autenticação inválido.' });
+  }
+}
